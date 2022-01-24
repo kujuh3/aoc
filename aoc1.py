26a4ce528a984375f19abe540a9cfbfbaba109c2
kjadev <kujala1@windowslive.com>
2022-01-24 20:55:30 +0200

    Day1: part 1 solution

---
+++ aoc1.py
@@ -0,0 +1,17 @@
+input = open("input.txt", "r")
+
+with open("input.txt") as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+z = 0
+
+for i, x in enumerate(lines):
+    if i == 0:
+        pass
+    else:
+        pre = int(lines[i-1])
+        if(int(x) > pre):
+            z+=1
+
+print(f"Total number of increases: {z}")
