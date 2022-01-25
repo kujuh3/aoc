94650a8742cab94768483e4daf55af816070f9e4
kjadev <kujala1@windowslive.com>
2022-01-25 04:59:56 +0200

    Day 1: part 1 and 2

--- aoc_1/aoc1.1.py
+++ aoc_1/aoc1.1.py
@@ -1,17 +1,21 @@
+#Open file, read lines and strip from line changes
 with open("input.txt") as file:
     lines = file.readlines()
     lines = [int(line.rstrip()) for line in lines]
 
-
+#Initial values for iteration, counter and preceding values
 z = 0
 n = 0
 i = 0
 
+#Just do it with a function
 def window(list, x):
     for i in range(len(lines)-x+1):
         yield lines[i:i+x]
 
+#Looping returned 3 element windows
 for s in window(lines, 3):
+    #Just to ignore first values, otherwise result will be 1 over real result
     i+=1
     #Ignore first iteration
     if i >= 2:
--- aoc_1/aoc1.py
+++ aoc_1/aoc1.py
@@ -1,12 +1,17 @@
+#Open file, read lines and strip from line changes
 with open("input.txt") as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
+#Initial variable for increases
 z = 0
 
+#For loop including element value and index
 for i, x in enumerate(lines):
+    #Ignore first iteration
     if i == 0:
         pass
+    #Get preceding value, compare to current and increase counter
     else:
         pre = int(lines[i-1])
         if(int(x) > pre):
