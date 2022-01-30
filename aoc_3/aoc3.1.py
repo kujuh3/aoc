1cc3065e2f81f71b629ca233cae6d1513da9995e
kjadev <66220187+kujuh3@users.noreply.github.com>
2022-01-30 18:09:21 +0200

    Update aoc3.1.py

--- aoc_3/aoc3.1.py
+++ aoc_3/aoc3.1.py
@@ -7,18 +7,22 @@ with open("input.txt") as file:
 
 i = 0
 #Why the fuck wont this work?
+#For each character in string, while loop
 while i < 12:
+    #Reset current iterations most common value
     c = 0
+    #For each line, count binary occurrences based on index
     for e in lines:
         if int(e[i]) == 1:
             c += 1
         elif int(e[i]) == 0:
             c -= 1
+    #Positive = 1, negative = 0
     if c > 0:
         c = 1
     else:
         c = 0
-
+    #Loop through lines again and if corresponding character at index isn't equal to most common value, remove it.
     for e in lines:
         if int(e[i]) != c:
             print(e[i])
